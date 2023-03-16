71886da87e219e333b7278eac075f73c62ff50d8
Kotka <kotkadkofficial@gmail.com>
2023-03-16 13:26:25 +0100

    commit

--- Main.py
+++ Main.py
@@ -6,17 +6,18 @@ import DataProcessing
 import Brain
 
 
+"""
 _BagOfWords = BagOfWords()
 
 # We now process all the data
 DataProcessing.ProcessAllData(_BagOfWords)
 
-"""
+
 for word in _BagOfWords.BoW.keys():
     print(f'{word} : {_BagOfWords.GetPostWords(word)}')
-"""
+
 
 prompt = random.choice(list(_BagOfWords.BoW.keys()))
-count = random.randint(1, 20)
-prompt = Brain.PredictSequenceNoContext(prompt, count, _BagOfWords)
 print(prompt)
+"""
+
