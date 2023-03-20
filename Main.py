1bc4801f521d612391c12e5d16e5fc84b9cad0c0
Kotka <kotkadkofficial@gmail.com>
2023-03-20 22:10:14 +0100

    Made it better

--- Main.py
+++ Main.py
@@ -4,24 +4,28 @@ import TextProcessing
 import DataProcessing
 import Brain
 from datetime import datetime
+import gc
 
 
 _BagOfWords = BagOfWords()
+
+# Step 0 : Enable garbage collection
+gc.enable()
 
 # Step 1 : We process all the data we have 
 for N in range(2, 6):
     print(f'Data processing for N={N} started! [{datetime.now().strftime("%H.%M:%S %d/%m/%Y")}]')
     DataProcessing.ProcessAllData(_BagOfWords, N)
+    gc.collect()
     print(f'Data processing for N={N} finished! [{datetime.now().strftime("%H.%M:%S %d/%m/%Y")}]')
     print("=" * 128)
 
 # Step 2 : Clean the bag
 print(f'Bag cleaning started! [{datetime.now().strftime("%H.%M:%S %d/%m/%Y")}]')
 DataProcessing.CleanBag(_BagOfWords)
+gc.collect()
 print(f'Bag cleaning finished! [{datetime.now().strftime("%H.%M:%S %d/%m/%Y")}]')
 print("=" * 128)
-
-print(len(_BagOfWords.BoW))
 
 # Prompt part
 prompt = input("Enter prompt: ")
